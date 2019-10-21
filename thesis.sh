7559267627e7d31666576013e72f386a9690b896
Rohan Dsouza <dsouza.rohan06@gmail.com>
2019-10-21 21:26:04 +0000

    First commit

--- thesis.sh
+++ thesis.sh
@@ -6,8 +6,25 @@
 #=========================================================================================================
 
 HOSTS='./hosts'
+PID='/tmp/lock.file'
+
+if [ -f "$PID" ]
+then
+	echo "Exiting from if loop"
+      	exit 2
+else
+	echo > $PID
+fi
 
 for i in `tail -n +2 $HOSTS`
 do
-echo $i
+	echo $i
 done
+
+
+
+
+
+
+
+rm -f $PID
